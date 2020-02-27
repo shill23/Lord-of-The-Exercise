7c8b43d89cd681ed3dbffe2f51ec39516d43f6d6
shill23 <60882860+shill23@users.noreply.github.com>
2020-02-27 11:19:43 -0600

    Update memberPlay.py

--- memberPlay.py
+++ memberPlay.py
@@ -8,13 +8,15 @@ import firebase_admin
 import json
 
 # Path to json file that stores my credentials
-cred = credentials.Certificate('/Users/simonehill/Desktop/PYTHON FILES/gymMembers/bobsrealKey.json')
+cred = credentials.Certificate('the path to your cred file.json')
 # Project ID
 project_id = 'bobsburgers-5d057'
 firebase_admin.initialize_app(cred, {
   'projectId': project_id,
 })
-uid = "KYPC041rbdcucKgSvPID8d5BJj83"
+# The userID
+theUID = input("Please enter the users ID")
+uid = theUID
 user = auth.get_user(uid)
 print('Successfully fetched user data: {0}'.format(user.email))
 
